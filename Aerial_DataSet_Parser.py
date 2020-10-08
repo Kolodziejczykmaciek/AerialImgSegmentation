5fa176b6d3d0506f3dbe708db9d7c6f8324125a0
Kolodziejczykmaciek <maciekk529@gmail.com>
2020-10-08 11:49:00 +0200

    Seg push

---
+++ Aerial_DataSet_Parser.py
@@ -0,0 +1,104 @@
+
+import random
+import numpy as np
+import os
+import cv2
+
+
+np.random.seed(1000)
+
+'''
+    Dowanload dataset useed in this project in https://project.inria.fr/aerialimagelabeling/
+    Keep in mind that it weights about 20Gb.
+'''
+
+def aerialDataPrepare(num_train_img):
+    '''
+        This method returns given number of training images and corresponding masks as a nd.array.
+        Images in data set are 5000x5000 pixels this method cuts every image and its binary mask into 512x512
+        training images.
+        One 5000x5000 pixel image is cut into 100 images.
+
+        For now the method is hardcoded to evaluate training 5000x5000 pixel images into 512x512 but
+        it can by transormed to return images in different resolution. As far as i konw the resolution must be always
+        divisible by 32.
+    '''
+
+    IMG_HEIGHT = 512
+    IMG_WIDTH = 512
+    IMG_CHANNELS = 3
+
+    X_train = np.zeros((num_train_img, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    Y_train = np.zeros((num_train_img, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+
+    image_directory = r'C:/Users/macie/PycharmProjects/pythonProject/aerial_dataSet/NEW2-AerialImageDataset/AerialImageDataset/train/images/'
+    mask_directory = r'C:/Users/macie/PycharmProjects/pythonProject/aerial_dataSet/NEW2-AerialImageDataset/AerialImageDataset/train/gt/'
+
+    cords = (0, 512, 1024, 1536, 2048, 2560, 3072, 3584, 4096, 4487)
+    image_resolution = 512
+
+    train_images = os.listdir(image_directory)
+    random.shuffle(train_images)
+    n = 0
+    for i, image_name in enumerate(train_images):
+        print("Interation no.{}".format(i))
+        if i == num_train_img/100: break
+        if '.tif' in image_name:
+            img = cv2.imread(image_directory + image_name)
+            label = cv2.imread(mask_directory + image_name)
+
+            # sub_image = 0
+            for i in cords:
+                for j in cords:
+                    new_img = img[i:i + image_resolution, j:j + image_resolution]
+                    X_train[n] = np.array(new_img)
+
+                    new_label = label[i:i + image_resolution, j:j + image_resolution]
+                    new_label = new_label[:, :, 0:1]
+                    new_label = new_label / 255
+                    Y_train[n] = new_label
+                    n+=1
+                    # sub_image+=1
+                    # new_img_name = image_name[0:-4]+'_{}'.format(sub_image)
+                    # cv2.imwrite(img_dir+new_img_name+".tif", new_img)
+
+    return X_train, Y_train
+
+def aerialValidationData(num_val_img):
+    '''
+        This method returns given number of 100xvalidation images as a nd.array.
+        Images in data set are 5000x5000 pixels this method cuts every image into 512x512 validation images.
+        One 5000x5000 pixel image is cut into 100 images.
+
+        For now the method is hardcoded to evaluate validation 5000x5000 pixel images into 512x512 but
+        it can by transormed to return images in different resolution. As far as i konw the resolution must be always
+        divisible by 32.
+    '''
+
+    IMG_HEIGHT = 512
+    IMG_WIDTH = 512
+    IMG_CHANNELS = 3 #RGB
+
+    X_val = np.zeros((num_val_img, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.float32)
+
+    image_directory = r'C:/Users/macie/PycharmProjects/pythonProject/aerial_dataSet/NEW2-AerialImageDataset/AerialImageDataset/test/images/' #specify path to validation images
+
+    cords = (0, 512, 1024, 1536, 2048, 2560, 3072, 3584, 4096, 4487)
+    image_resolution = 512
+
+    validation_images = os.listdir(image_directory)
+    random.shuffle(validation_images)
+    print(validation_images)
+    n = 0
+    for i, image_name in enumerate(validation_images):
+        print("Interation no.{}".format(i))
+        if i == num_val_img/100: break
+        if '.tif' in image_name:
+            img = cv2.imread(image_directory + image_name)
+
+            for i in cords:
+                for j in cords:
+                    new_img = img[i:i + image_resolution, j:j + image_resolution]
+                    X_val[n] = np.array(new_img)
+                    n+=1
+    return X_val
---
+++ Segmentaion train evaluate display.py
@@ -0,0 +1,88 @@
+import sys
+import time
+import tensorflow as tf
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+
+from Aerial_DataSet_Parser import aerialDataPrepare
+from Aerial_DataSet_Parser import aerialValidationData
+from Unet_Model import UnetModel
+
+seed = 42
+np.random.seed = seed
+
+
+# X_train, Y_train = aerialDataPrepare(1000) #get data
+# print("Dl listy X_train: {} \nDl listy Y_train: {}".format(len(X_train), len(Y_train)))
+
+
+X_val = aerialValidationData(1000) # get validation data
+#
+# for i in range(20): # Can use this loop to look through loaded train/val images
+#
+#     ix = random.randint(0, len(X_val) - 1)
+#     fig, (ax1, ax2) = plt.subplots(1, 2)
+#     fig.suptitle('Zdjecie z listy numer:  {}'.format(ix), fontsize=15)
+#
+#     im1 = ax1.imshow(X_val[ix])
+#     ax1.set_title('Zdj, którego sieć nie zna')
+#     im2 = ax2.imshow(np.squeeze(X_val[ix]))
+#     ax2.set_title('Maska binarna z {}'.format("pies"))
+#     plt.show()
+#
+#
+'''
+    Training of our Unet model.
+    Imortant parameters: patience, batch_size, epochs, etc.
+'''
+
+# checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_nuclei.h5', verbose=1, save_best_only=True)
+# model = UnetModel() #get the model
+# callbacks = [
+#     tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+#     tf.keras.callbacks.TensorBoard(log_dir='logs')]
+#
+# results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=10, epochs=100, callbacks=callbacks)
+# model.save(r"C:\Users\macie\PycharmProjects\pythonProject\AerialDataSet_p2_bs10_4ktrainimg") #to save the model
+
+
+
+'''
+------------Evaluate on already trained model--------------
+    
+    -Load model from save
+    -get validation data
+    -plot semgentated images
+'''
+print("Loading model from save...")
+model = tf.keras.models.load_model(r"C:/Users/macie/PycharmProjects/AerialImgSegmentation/Trained_models/AerialDataSet_p2_bs10_4ktrainimg")
+print("Model has been loaded!")
+
+X_val = X_val[0:1]
+try:
+    time_start = time.time()
+    print('Segmentaion of {} images in progres...'.format(len(X_val)))
+    predition_Xval = model.predict(X_val, verbose=1)
+    time_stop = time.time()
+    print("It took: {} seconds".format(time_stop-time_start))
+except MemoryError:
+    print("Za mało ramu !!!")
+    sys.exit()
+
+for n in range(1):
+    ix = random.randint(0, len(X_val)-1)
+    # Perform a sanity check on some random training samples
+    # Display some validation images
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle('Segmentation of aerial images', fontsize=15)
+
+    im1 = ax1.imshow(np.squeeze(X_val[0].astype(np.uint8)))
+    ax1.set_title('Image that neural net has never seen')
+    # im2 = ax2.imshow(np.squeeze(Y_val[ix]))
+    # ax2.set_title('Maska binarna z {}'.format(classes))
+    im2 = ax2.imshow(np.squeeze(predition_Xval[0]))
+    ax2.set_title('Evaluated result')
+    plt.show()
